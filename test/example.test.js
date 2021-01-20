e53828184cb6ea191138b8b0469fecb8116ebd04
Dominique Sakrisson <domsakrisson@gmailcom>
2021-01-19 18:08:23 -0800

    MAKE TESTS PASS, LINKS BETWEEN HOMEPAGE AND SHOP

--- test/example.test.js
+++ test/example.test.js
@@ -1,18 +1,19 @@
 // IMPORT MODULES under test here:
-// import { example } from '../example.js';
+import { renderCars } from '../products/render-cars.js';
+import { cars } from '../data/data.js';
 
 const test = QUnit.test;
 
 test('time to test a function', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = true;
+    const expected = '<li class="economy" title="An economic little get-around-mobile">Toyota Corolla<img src="../assets/toyota.jpg" class="car-img"><p>An economic little get-around-mobile</p><p>$22500</p><button>Add to cart</button></li>';
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = false;
+    const actual = renderCars(cars[0]);
 
     //Expect
     // Make assertions about what is expected versus the actual result
-    expect.equal(actual, expected);
+    expect.equal(actual.outerHTML, expected);
 });
